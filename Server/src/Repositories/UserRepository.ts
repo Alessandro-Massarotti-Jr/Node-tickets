f5d5d34c219106ed7b04b8181b12c5c44ce51c53
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-09-20 20:57:21 -0300

    user repository

--- Server/src/Repositories/UserRepository.ts
+++ Server/src/Repositories/UserRepository.ts
@@ -4,23 +4,36 @@ import bcrypt from "bcrypt"
 const prisma = new PrismaClient();
 
 export interface UserInterface {
-    id:string;
-    name:string;
-    email:string;
-    password:string;
+    id: string;
+    name: string;
+    email: string;
+    password: string;
 }
 
-export class UserRepository{
+export class UserRepository {
 
-    public find(){
+    public async find(user_id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: user_id
+            },
+            select: {
+                email: true,
+                name: true,
+                id: true
+            }
+        });
+
+        async () => { await prisma.$disconnect(); };
+
+        return user;
+    }
+
+    public search() {
 
     }
 
-    public search(){
-
-    }
-    
-    public async store(user: Omit<UserInterface,'id'>){
+    public async store(user: Omit<UserInterface, 'id'>) {
 
         const salt = await bcrypt.genSalt(12);
         const passwordHash = await bcrypt.hash(user.password, salt);
@@ -44,16 +57,39 @@ export class UserRepository{
 
     }
 
-    public update(){
+    public update() {
 
     }
 
-    public delete(){
+    public async delete(user_id : string) {
+        const deletedUser = await prisma.user.delete({
+          where:{
+            id:user_id
+          },
+          select: {
+            email: true,
+            name: true,
+            id: true
+        }
+        });
 
+        async () => { await prisma.$disconnect(); };
+
+        return deletedUser;
     }
 
-    public findAll(){
+    public async findAll() {
+        const users = await prisma.user.findMany({
+            select: {
+                email: true,
+                name: true,
+                id: true
+            }
+        });
 
+        async () => { await prisma.$disconnect(); };
+
+        return users;
     }
 
 }
